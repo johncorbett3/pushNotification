c3f6ce06699a6f378cf8255bc618e231192b9a6b
John Corbett <john.corbett@kellogg.northwestern.edu>
2022-09-23 10:53:48 -0400

    changed email

--- push.js
+++ push.js
@@ -5,7 +5,7 @@ let vapidKeys = {
     privateKey: '0uTUd_h_FaGYRnWHKdLw8SszIOoqcY0NMpKYawzh7Os'
   }
 
-push.setVapidDetails('mailto@john.corbett3@yahoo.com', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails('mailto:john.corbett3@yahoo.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
 let sub = {
   endpoint:"https://fcm.googleapis.com/fcm/send/f5drH9Iju-c:APA91bEQUBKdRZr4jDJTJCchDlQgYM_xbgLaLNtNugO2xe5IfiJdg7XWRfKpdJX2kvQMxrorDtp_1mm3AGUCPxhRfT5D7IXsJ_URmLp08O0Y5CmdsQH9Vamd2aft4CaTSKIgOIha7l7e",
