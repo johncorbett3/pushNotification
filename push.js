f3dd1c098bbc1e9d9eee2e5fde22c6384c5c3415
John Corbett <john.corbett@kellogg.northwestern.edu>
2022-09-23 10:49:37 -0400

    added key from client

--- push.js
+++ push.js
@@ -7,6 +7,12 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto@john.corbett3@yahoo.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+  endpoint:"https://fcm.googleapis.com/fcm/send/f5drH9Iju-c:APA91bEQUBKdRZr4jDJTJCchDlQgYM_xbgLaLNtNugO2xe5IfiJdg7XWRfKpdJX2kvQMxrorDtp_1mm3AGUCPxhRfT5D7IXsJ_URmLp08O0Y5CmdsQH9Vamd2aft4CaTSKIgOIha7l7e",
+  expirationTime:null,
+  keys:{
+    p256dh:"BCiGc8Glyw1vBAKG24AZoKgyeCG-dngkhNs2TFWKo3aLQKiChXgQ3n7dSrkrTKySKm7j6Qutx2_ONKaJCU2l9AU",
+    auth:"H-h5MsEY9JPHN7RvEFj-Hg"
+  }};
 
 push.sendNotification(sub, 'test message')
